8ed49e1df1bad3f966f5b999e345a2f6928e223e
DevikaSVinod <devika.vinod@msds.christuniversity.in>
2024-08-29 08:54:53 +0530

    update

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import cart,checkout,shop_detail,shop
 
+# urls
 urlpatterns = [
     path('', cart, name='cart'),
     path('about', checkout, name='checkout'),
     path('contact', shop_detail, name='shop-detail'),
     path('gallery', shop, name='shop'),
-     
+
 ]
 
