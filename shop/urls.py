36854f9536e67130c1f47d62a0984684a48fa995
DevikaSVinod <devika.vinod@msds.christuniversity.in>
2024-08-29 08:35:29 +0530

    update5

---
+++ shop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import cart,checkout,shop_detail,shop
+
+urlpatterns = [
+    path('', cart, name='cart'),
+    path('about', checkout, name='checkout'),
+    path('contact', shop_detail, name='shop_detail'),
+    path('gallery', shop, name='shop'),
+    
+]
+
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cart(request):
+    return render(request, 'cart.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def shop_detail(request):
+    return render(request,'shop-detail.html')
+
+def shop(request):
+    return render(request,'shop.html')
+
