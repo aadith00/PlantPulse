e8bceeffe6d3d28e4bf1253f380024e7c0b2ed14
DevikaSVinod <devika.vinod@msds.christuniversity.in>
2024-08-29 08:53:44 +0530

    update

--- shop/urls.py
+++ shop/urls.py
@@ -4,8 +4,8 @@ from .views import cart,checkout,shop_detail,shop
 urlpatterns = [
     path('', cart, name='cart'),
     path('about', checkout, name='checkout'),
-    path('contact', shop_detail, name='shop_detail'),
+    path('contact', shop_detail, name='shop-detail'),
     path('gallery', shop, name='shop'),
-    
+     
 ]
 
