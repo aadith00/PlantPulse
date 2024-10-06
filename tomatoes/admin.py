3fb6f408426d04f3ba345e217530fab06f384d87
DevikaSVinod <devika.vinod@msds.christuniversity.in>
2024-10-06 15:51:33 +0530

    admin.py-tomatoes

--- tomatoes/admin.py
+++ tomatoes/admin.py
@@ -8,3 +8,27 @@ admin.site.register(ProductImages)
 admin.site.register(CartOrder)
 admin.site.register(CartOrderItems)
 admin.site.register(Address)
+
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesAdmin]
+    list_display = ['user','title','product_image','price','featured','product_status']
+
+class CartOrderAdmin(admin.ModelAdmin):
+    list_display = ['user','price','paid_status','order_date','product_status']
+
+class CartOrderItemsAdmin(admin.ModelAdmin):
+    list_display = ['order','invoice_no','item','image','qty','price','total']
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user','address','status']
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Product,CartOrderAdmin)
+admin.site.register(Product,CartOrderItemsAdmin)
+admin.site.register(Product,AddressAdmin)
+
+
