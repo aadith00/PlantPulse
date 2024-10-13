24b7e61feb321bf8beec0e024c2c040595c8958e
Aadith Joseph Mathew <maadith2002@gmail.com>
2024-10-13 22:20:13 +0530

    Models

--- account/admin.py
+++ account/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
-from .models import Farmer,Profile
+from .models import Farmer,Profile, BillingAddress
 
 class FarmerAdmin(admin.ModelAdmin):
     list_display = ['name','image','contact']
 
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['username','email']
+# class CustomerAdmin(admin.ModelAdmin):
+#     list_display = ['username','email']
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['full_name','phone']
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'city']
 
 # Register your models here.
 admin.site.register(Farmer,FarmerAdmin)
 # admin.site.register(Customer,CustomerAdmin)
 admin.site.register(Profile,ProfileAdmin)
+admin.site.register(BillingAddress,AddressAdmin)
