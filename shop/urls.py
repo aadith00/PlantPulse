6bbf174c9089692663cfd8a3f747f52b1f47ebeb
DevikaSVinod <devika.vinod@msds.christuniversity.in>
2024-10-31 20:56:55 +0530

    payment

--- shop/urls.py
+++ shop/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import checkout,shop_detail, product_detail, add_to_cart, cart, remove_from_cart
+from .views import checkout,shop_detail, product_detail, add_to_cart, cart, remove_from_cart, checkoutpage, payment
 
 #urls
 urlpatterns = [
     path('cart/', cart, name='cart'),
-    path('checkout', checkout, name='checkout'),
+    path('checkoupage', checkoutpage , name='checkoutpage'),
     path('shopdetail', shop_detail, name='shop-detail'),
     path('product/<pid>/', product_detail, name='product-detail'),
     path("add-to-cart/", add_to_cart, name="add-to-cart"),
     path('remove-from-cart/<int:item_id>/', remove_from_cart, name='remove-from-cart'),
     path('checkout/', checkout, name='checkout'),
+    path('payment/', payment, name='payment'),
 ]
